561232f511961db0469f2e7e88bea49afdf57bb5
ecnopdev <ponce.josemari@gmail.com>
2020-06-17 11:35:30 +0800

    removed space

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,7 +3,6 @@
     <v-form>
       <v-text-field label="Username"></v-text-field>
       <v-text-field label="Password"></v-text-field>
-
       <div class="d-flex flex-column flex-sm-row">
         <v-btn class="my-2 pa-5">Login</v-btn>
         <v-btn class="my-2 pa-5" color="blue" dark>Login with Facebook</v-btn>
