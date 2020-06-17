1bf32db541959713cc98c03c0619985f1f4adde2
ecnopdev <ponce.josemari@gmail.com>
2020-06-17 09:22:14 +0800

    update build files

---
+++ docs/precache-manifest.1efdf34032fff1404d49777e19181bad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018daf7aabbd8993cf80",
+    "url": "/css/app.0e88525e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "364746b88a8e5375fee4cfed03dfb61b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9f5a665a3c323f2f9bb",
+    "url": "/js/about.5c369863.js"
+  },
+  {
+    "revision": "018daf7aabbd8993cf80",
+    "url": "/js/app.f2a80bbf.js"
+  },
+  {
+    "revision": "a10e28744c68f701947d",
+    "url": "/js/chunk-vendors.09fdc3f8.js"
+  },
+  {
+    "revision": "0dbbb301143c45533e4eb8bdaf6fcb0f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
